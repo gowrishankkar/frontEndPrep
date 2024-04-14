b100979a04025bbfa6b2a62c6beb468001d6be68
GSP <Gowri.Shankar@tecnotree.com>
2024-04-14 13:43:20 +0530

    Grid Lights

--- src/GridLights/GridLights.jsx
+++ src/GridLights/GridLights.jsx
@@ -1,10 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+import "./GridLights.css";
+
+const Cell = ({ filled, onClick, isDisabled, label }) => {
+  return (
+    <button
+      type="button"
+      aria-label={label}
+      disabled={isDisabled}
+      onClick={onClick}
+      className={filled ? "cell cell_activated" : "cell"}
+    />
+  );
+};
 
 const GridLights = () => {
+  const [order, setOrder] = useState([]);
+  const [isDeactivating, setIsDeactivating] = useState(false);
+  const config = [
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 1, 1],
+  ];
+
+  const activateCells = (index) => {
+    const newOrder = [...order, index];
+    setOrder(newOrder);
+    if (newOrder.length === config.flat().filter(Boolean).length) {
+      deactivateCells();
+    }
+  };
+
+  const deactivateCells = () => {
+    setIsDeactivating(true);
+    const timer = setInterval(() => {
+      setOrder((origOrder) => {
+        const newOrder = origOrder.slice();
+        newOrder.pop();
+
+        if (newOrder.length === 0) {
+          clearInterval(timer);
+          setIsDeactivating(false);
+        }
+
+        return newOrder;
+      });
+    }, 300);
+  };
+
   return (
-    <>
-      <h1>GridLights</h1>
-    </>
+    <div className="wrapper">
+      <div
+        className="grid"
+        style={{ gridTemplateColumns: `repeat(${config[0].length}, 1fr)` }}
+      >
+        {config.flat().map((value, index) => {
+          return value ? (
+            <Cell
+              key={index}
+              label={`Cell ${index}`}
+              filled={order.includes(index)}
+              isDisabled={order.includes(index) || isDeactivating}
+              onClick={() => activateCells(index)}
+            />
+          ) : (
+            <span></span>
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
